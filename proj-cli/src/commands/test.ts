bda4463a9886a6dd028c43c9151edf632275c17d
eetu pursiainen <eepeli42@gmail.com>
2021-01-11 16:30:26 +0200

    first test version with base and a few test commands

---
+++ proj-cli/src/commands/test.ts
@@ -0,0 +1,26 @@
+import { Command, flags } from "@oclif/command"
+
+export default class Test extends Command {
+  static description = 'test'
+
+  static examples = [
+    `test --name=eetu`,
+  ]
+
+  static flags = {
+    name: flags.string({
+      char: "n",
+      description: "your name"
+    }),
+    help: flags.help({char: 'h'})
+  }
+
+  /**
+   * Function that returns a greeting
+   */
+  public run = async () => {
+    const { flags } = this.parse(Test);
+    const name = flags.name ? flags.name : "unknown";
+    this.log(`Hello ${name}, how are you doing?`);
+  }
+}
