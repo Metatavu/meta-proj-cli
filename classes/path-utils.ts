809d2b0cac7afeaa7eb52c2bf7052f96ba59b900
veikkahonkanen <aveho002@edu.xamk.fi>
2021-02-03 12:12:14 +0200

    home path now creates itself and works on each os

--- classes/path-utils.ts
+++ classes/path-utils.ts
@@ -1,6 +1,10 @@
 import * as path from "path";
+import fs from "fs";
+import { execSync } from "child_process";
 
 const { HOME } = process.env;
+const defaultSavePath : string = "~/.meta-proj-cli/";
+const defaultProjectPath : string = "~/.meta-proj-cli/projects";
 
 /**
  * Offers functions that help with paths
@@ -10,18 +14,12 @@ const { HOME } = process.env;
  * repoFolder : gives the path format to the folder that actually holds the project (contains .git)
  */
 export class PathUtils {
-  static fixPath = (givenPath : string) => {
-    givenPath = path.join(...givenPath.split(/\/|\\/));
-  
-    if (givenPath[0] === "~") {
-      givenPath = path.join(HOME, givenPath.slice(1))
-    } 
-  
-    if (givenPath[0] !== "/") {
-      givenPath = path.sep + givenPath;
-    } 
-  
-    return givenPath;
+  static savePath : string = pathFixer(defaultSavePath);
+
+  static projectPath : string = pathFixer(defaultProjectPath);
+
+  static fixPath = (givenPath : string) => { 
+    return pathFixer(givenPath);
   }
 
   static outerFolder = (givenPath : string, repoName : string) => {
@@ -31,4 +29,33 @@ export class PathUtils {
   static repoFolder = (givenPath : string, repoName : string) => {
     return path.join(givenPath, repoName + "-project", repoName);
   }
+
+  static checkExists = async (givenPath : string) => {
+    try {
+      givenPath = pathFixer(givenPath);
+      if (!fs.existsSync(givenPath)) {
+        execSync(`mkdir ${givenPath}`);
+      }
+    } catch(err) {
+      throw new Error(`Error when checking or creating path: ${err}`);
+    }
+  }
 }
+
+function pathFixer(givenPath : string) : string {
+  
+  if (givenPath[0] === "~") {
+    givenPath = path.join(HOME, givenPath.slice(1))
+  }
+
+  if (givenPath[0] === "C" || givenPath[0] === "D" || givenPath[0] === "E") {
+    givenPath = path.join(...givenPath.split(/\/|\\/));
+  }
+
+  else {
+    givenPath = path.join(...givenPath.split(/\/|\\/));
+    givenPath = path.sep + givenPath;
+  }
+  return givenPath;
+}
+
