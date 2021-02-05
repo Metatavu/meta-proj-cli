0a52c730f133bc7158ccc882b4e373970696f471
veikkahonkanen <49862032+veikkahonkanen@users.noreply.github.com>
2021-02-05 11:27:34 +0200

    Update classes/os-commands.ts
    Co-authored-by: Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>

--- classes/os-commands.ts
+++ classes/os-commands.ts
@@ -1,24 +1,25 @@
 import { OsCommand } from "../interfaces/cmd-command";
 
 const cmds : OsCommand[] = [
-{
-  OS : "WINDOWS",
-  commands: [
-    {name : "copy", command : "copy"}
-  ] 
-},
-{
-  OS : "LINUX",
-  commands: [
-    {name : "copy", command : "cp"}
-  ] 
-},
-{
-  OS : "MAC OS",
-  commands: [
-    {name : "copy", command : "cp"}
-  ] 
-}];
+  {
+    OS : "WINDOWS",
+    commands: [
+      { name : "copy", command : "copy" }
+    ] 
+  },
+  {
+    OS : "LINUX",
+    commands: [
+      { name : "copy", command : "cp" }
+    ] 
+  },
+  {
+    OS : "MAC OS",
+    commands: [
+      { name : "copy", command : "cp" }
+    ] 
+  }
+];
 
 export class OsCommands {
   static getCmds = () : OsCommand[] => {
