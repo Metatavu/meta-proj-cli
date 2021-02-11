fb5e7b7bab5f0ae9453f2094f83be1940ae95b03
veikkahonkanen <49862032+veikkahonkanen@users.noreply.github.com>
2021-02-11 12:51:02 +0200

    Update src/classes/os-utils.ts
    Co-authored-by: Antti Leppä <antti.leppa@metatavu.fi>

--- src/classes/os-utils.ts
+++ src/classes/os-utils.ts
@@ -1,7 +1,7 @@
 import { UserConfigJson, OperatingSystems } from "../interfaces/types";
 import { OsCommands } from "./os-commands";
 import fs from "fs";
-import {windows, linux, macos} from 'platform-detect';
+import { windows, linux, macos } from 'platform-detect';
 
 const systems : string[]  = [OperatingSystems.MAC, OperatingSystems.WINDOWS, OperatingSystems.LINUX];
 
