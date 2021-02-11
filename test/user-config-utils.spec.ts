c1bba5903e35262252b3616a4025d11b918c8f25
eetu pursiainen <eepeli42@gmail.com>
2021-02-11 15:54:09 +0200

    made recommended changes

--- test/user-config-utils.spec.ts
+++ test/user-config-utils.spec.ts
@@ -1,10 +1,6 @@
 import { UserConfigUtils } from "../src/classes/user-config-utils";
 import { UserConfigJson } from "../src/interfaces/types";
 
-const mockUserConfig : UserConfigJson = {
-  "osPref": "LINUX"
-}
-
 it("reads user-config.json", async () => {
   const data : UserConfigJson = await UserConfigUtils.readUserConfig();
 
@@ -13,7 +9,11 @@ it("reads user-config.json", async () => {
 
 it("is able to write to user-config-json", async () => {
   UserConfigUtils.writeUserConfig = jest.fn();
-  UserConfigUtils.writeUserConfig(JSON.stringify(mockUserConfig));
+  UserConfigUtils.writeUserConfig(JSON.stringify(
+    {
+    "osPref": "LINUX"
+    }
+  ));
 
   expect(UserConfigUtils.writeUserConfig).toReturn();
 });
