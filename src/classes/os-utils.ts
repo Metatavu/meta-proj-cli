c0a3c5c966957f1930a94dfbbbb5f5d1a8c48ae9
eetu pursiainen <eepeli42@gmail.com>
2021-02-12 11:24:16 +0200

    appeased the sonar gods (removed unused import)

--- src/classes/os-utils.ts
+++ src/classes/os-utils.ts
@@ -1,7 +1,6 @@
 import { UserConfigJson, OperatingSystems } from "../interfaces/types";
 import { OsCommands } from "./os-commands";
 import { UserConfigUtils } from "./user-config-utils";
-import fs from "fs";
 import { windows, linux, macos } from 'platform-detect';
 
 const systems : string[]  = [OperatingSystems.MAC, OperatingSystems.WINDOWS, OperatingSystems.LINUX];
