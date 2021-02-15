eb55858c23fc731d10947be3f8cb373602c1009b
veikkahonkanen <aveho002@edu.xamk.fi>
2021-02-15 15:11:57 +0200

    Added Quarkus creation function

---
+++ src/commands/new-proj/create-quarkus.ts
@@ -0,0 +1,27 @@
+/**
+ * Constructs creation command for Quarkus projects
+ * @param projName desired project name
+ * 
+ * @param kotlin true/false if using Kotlin
+ * 
+ * @param gradle true/false if using Gradle
+ * 
+ * @returns a command that is used to create the Quarkus project
+ */
+export const CreateQuarkus = async (projName : string, kotlin : boolean, gradle : boolean) : Promise<string> => {
+  const orgName = "fi.metatavu";
+  let kotlinS : string = null;
+  kotlin ? kotlinS = ", kotlin" : kotlinS = "";
+  let gradleS : string = null;
+  gradle ? gradleS = "\
+  -DbuildTool=gradle" : gradleS = "";
+
+  const bashCmd = `mvn io.quarkus:quarkus-maven-plugin:1.11.3.Final:create \
+  -DprojectGroupId=${orgName} \
+  -DprojectArtifactId=${projName} \
+  -DclassName="${orgName}.${projName}" \
+  -Dpath="/${projName}" \
+  -Dextensions="resteasy, resteasy-jackson${kotlinS}" ${gradleS}`;
+  
+  return bashCmd;
+}
