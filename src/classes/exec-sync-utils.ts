bb5d71c5d1a3faff5b950ab35f9816cc44d4d12a
eetupur <77275251+eetupur@users.noreply.github.com>
2021-02-18 10:47:56 +0200

    Update src/classes/exec-sync-utils.ts
    Co-authored-by: veikkahonkanen <49862032+veikkahonkanen@users.noreply.github.com>

--- src/classes/exec-sync-utils.ts
+++ src/classes/exec-sync-utils.ts
@@ -5,7 +5,6 @@ import { execSync, ExecSyncOptionsWithBufferEncoding } from "child_process";
  * 
  * @param {string} command command to be run in shell
  * @param {ExecSyncOptionsWithBufferEncoding} options options to be provided to the shell
- * 
  * @returns {Promise<string | void>} if command outputs, will provide it as string
  */
 export const runExecSync = async (command: string, options?: ExecSyncOptionsWithBufferEncoding): Promise<string | void> => {
@@ -15,4 +14,3 @@ export const runExecSync = async (command: string, options?: ExecSyncOptionsWith
     throw new Error(`Error while executing command ${command}, with options ${options}. Error: ${err}`);
   }
 }
-
