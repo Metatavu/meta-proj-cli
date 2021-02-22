8fb534885bfc59f8b3f96a9497d9f69cf8377972
= <=>
2021-02-22 11:18:16 +0200

    Added Docker, Minikube, Kubernetes CLI

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -15,7 +15,7 @@ async function action(args){
       const softwareResult = await this.prompt({
         type : 'list',
         name : 'software',
-        choices : ['NodeJs', 'GitHub', 'Git CLI', 'Maven', 'Java Development Kit 8', 'Java Development Kit 11', 'Homebrew', 'Kustomize'],
+        choices : ['NodeJs', 'GitHub', 'Git CLI', 'Maven', 'Java Development Kit 8', 'Java Development Kit 11', 'Homebrew', 'Docker', 'Minikube', 'Kubernetes CLI', 'Kustomize'],
         message : "Software to be installed: "
       });
       if (softwareResult.name) {
@@ -48,6 +48,6 @@ export const installWizard = (vorpal: Vorpal): Vorpal.Command => vorpal
   .option(
     '-s, --software <type>',
     'specify the software to install, leave empty to enter wizard',
-    ['NodeJs', 'GitHub', 'Git CLI', 'Maven', 'Java Development Kit 8', 'Java Development Kit 11', 'Homebrew', 'Kustomize']
+    ['NodeJs', 'GitHub', 'Git CLI', 'Maven', 'Java Development Kit 8', 'Java Development Kit 11', 'Homebrew', 'Docker', 'Minikube', 'Kubernetes CLI', 'Kustomize']
   )
   .action(action);
