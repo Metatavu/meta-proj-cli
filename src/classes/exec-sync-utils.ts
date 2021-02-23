9d1fcb280963c3c74cdbc18189b2d626573fc3f3
= <=>
2021-02-23 11:04:04 +0200

    Refuses to execute sh or exe files

--- src/classes/exec-sync-utils.ts
+++ src/classes/exec-sync-utils.ts
@@ -1,7 +1,7 @@
 import { execSync, ExecSyncOptionsWithBufferEncoding } from "child_process";
 
 /**
- * runs the given command in a shell, vulnerable to injection
+ * runs the given command in a shell, denies attempts of injection
  * 
  * @param {string} command command to be run in shell
  * @param {ExecSyncOptionsWithBufferEncoding} options options to be provided to the shell
@@ -9,7 +9,8 @@ import { execSync, ExecSyncOptionsWithBufferEncoding } from "child_process";
  */
 export const runExecSync = async (command: string, options?: ExecSyncOptionsWithBufferEncoding): Promise<string | void> => {
   try {
-    return execSync(command, options)?.toString(); //NOSONAR
+    if (command.search(/;|\.sh$|\.exe$/) == -1) return execSync(command, options)?.toString(); //NOSONAR
+    
   } catch (err) {
     throw new Error(`Error while executing command ${command}, with options ${options}. Error: ${err}`);
   }
