88c0eadde09edd89a164689e526b9334642edb98
= <=>
2021-02-23 08:06:26 +0200

    Added Create Quarkus tests

---
+++ test/create-quarkus.spec.ts
@@ -0,0 +1,18 @@
+
+it('returns a Maven command string', async () => {
+  const str : string = await CreateQuarkus('test', false, false);
+
+  expect((str)).toMatch(/^mvn io.quarkus:quarkus-maven-plugin/);
+});
+
+it('applies Kotlin setting', () => {
+  const str : string = await CreateQuarkus('test', true, false);
+
+  expect((str)).toMatch(/kotlin/);
+});
+
+it('applies Gradle setting', () => {
+  const str : string = await CreateQuarkus('test', true, true);
+
+  expect((str)).toMatch(/gradle/);
+});
