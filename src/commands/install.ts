596a942287783c6fa5ecbdaec69397fd07bae048
Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>
2021-02-23 19:42:03 +0200

    Update src/commands/install.ts

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -10,7 +10,7 @@ import Vorpal from "vorpal";
 async function action(args){
   let software: string = args.options.software;
 
-  if(!software){
+  if (!software) {
     try {
       const softwareResult = await this.prompt({
         type : 'list',
