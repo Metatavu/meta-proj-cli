7ae6ffc2918cb00d810bc111fad5ca8548fae3eb
= <=>
2021-02-24 08:05:33 +0200

    Applied suggested changes

--- src/commands/new-proj/create-react.ts
+++ src/commands/new-proj/create-react.ts
@@ -18,26 +18,27 @@ export const CreateReact = async (projName: string): Promise<string> => {
  * @returns an array of strings which includes the commands to be run on the new-proj thread
  */
 export const CleanReact = async (folderPath: string, repoPath: string): Promise<string[]> => {
-  let remove : string = null;
-  let copy : string = null;
-  try {
-    remove = await OsUtils.getCommand(CommandNames.remove);
-    copy = await OsUtils.getCommand(CommandNames.copy);
-  } catch (err) {
-    throw new Error(`Error when fetching command: ${err}`);
-  }
-  const cmdsArray : string[] = [];
-  cmdsArray.push(`${remove} README.md`);
-  cmdsArray.push(`${remove} src/App.css`);
-  cmdsArray.push(`${remove} src/App.test.tsx`);
-  cmdsArray.push(`${remove} src/App.tsx`);
-  cmdsArray.push(`${remove} src/index.css`);
-  cmdsArray.push(`${remove} src/index.tsx`);
-  cmdsArray.push(`${remove} src/logo.svg`);
-  cmdsArray.push(`${remove} src/react-app-env.d.ts`);
-  cmdsArray.push(`${remove} src/setupTests.ts`);
-  cmdsArray.push(`${copy} project-config.json ${folderPath}`);
-  cmdsArray.push(`${copy} README.md ${repoPath}`);
+  const remove: string = await OsUtils.getCommand(CommandNames.remove);
+  const copy: string = await OsUtils.getCommand(CommandNames.copy);
 
-  return cmdsArray;
+  try {
+    const cmdsArray: string[] = [];
+    cmdsArray.push(`${remove} README.md`);
+    cmdsArray.push(`${remove} src/App.css`);
+    cmdsArray.push(`${remove} src/App.test.tsx`);
+    cmdsArray.push(`${remove} src/App.tsx`);
+    cmdsArray.push(`${remove} src/index.css`);
+    cmdsArray.push(`${remove} src/index.tsx`);
+    cmdsArray.push(`${remove} src/logo.svg`);
+    cmdsArray.push(`${remove} src/react-app-env.d.ts`);
+    cmdsArray.push(`${remove} src/setupTests.ts`);
+    cmdsArray.push(`${copy} project-config.json ${folderPath}`);
+    cmdsArray.push(`${copy} README.md ${repoPath}`);
+
+    return cmdsArray;
+
+  } catch (err) {
+    return Promise.reject(err);
+  }
+  
 }
